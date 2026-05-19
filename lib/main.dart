a2d74b3c1ef4d242e973686fcc230cd9709573b6
Zoran Luledzija <zoran.luledzija@outlook.com>
2026-05-19 12:28:32 +0200

    Update codebase with the latest changes

--- lib/main.dart
+++ lib/main.dart
@@ -101,10 +101,9 @@ class _MyHomePageState extends State<MyHomePage> {
       (response) => setState(() {
         _isLoading = false;
       }),
-      onError:
-          (error) => setState(() {
-            _isLoading = false;
-          }),
+      onError: (error) => setState(() {
+        _isLoading = false;
+      }),
     );
   }
 
@@ -129,44 +128,42 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child:
-            _isLoading
-                ? const CircularProgressIndicator()
-                : Column(
-                  // Column is also a layout widget. It takes a list of children and
-                  // arranges them vertically. By default, it sizes itself to fit its
-                  // children horizontally, and tries to be as tall as its parent.
-                  //
-                  // Column has various properties to control how it sizes itself and
-                  // how it positions its children. Here we use mainAxisAlignment to
-                  // center the children vertically; the main axis here is the vertical
-                  // axis because Columns are vertical (the cross axis would be
-                  // horizontal).
-                  //
-                  // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-                  // action in the IDE, or press "p" in the console), to see the
-                  // wireframe for each widget.
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      child: Text(
-                        S.of(context).pageHomeWelcomeMessage,
-                        style: const TextStyle(fontSize: 16),
-                      ),
+        child: _isLoading
+            ? const CircularProgressIndicator()
+            : Column(
+                // Column is also a layout widget. It takes a list of children and
+                // arranges them vertically. By default, it sizes itself to fit its
+                // children horizontally, and tries to be as tall as its parent.
+                //
+                // Column has various properties to control how it sizes itself and
+                // how it positions its children. Here we use mainAxisAlignment to
+                // center the children vertically; the main axis here is the vertical
+                // axis because Columns are vertical (the cross axis would be
+                // horizontal).
+                //
+                // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+                // action in the IDE, or press "p" in the console), to see the
+                // wireframe for each widget.
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    child: Text(
+                      S.of(context).pageHomeWelcomeMessage,
+                      style: const TextStyle(fontSize: 16),
                     ),
-                    Text(S.of(context).pageHomePushedButtonMessage(_counter)),
-                  ],
-                ),
+                  ),
+                  Text(S.of(context).pageHomePushedButtonMessage(_counter)),
+                ],
+              ),
       ),
-      floatingActionButton:
-          _isLoading
-              ? null
-              : FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: const Icon(Icons.add),
-              ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: _isLoading
+          ? null
+          : FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
